603e28f5de7b334e2046845dcc59f0491ac7fef3
alcantara09 <alcantara@poli.ufrj.br>
2015-08-19 12:20:30 -0300

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,18 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace laserscanner_sick_lms1xx
+{
+    class DummyClass
+    {
+        public: 
+            /**
+             * Print a welcome to stdout
+             * \return nothing
+             */
+            void welcome();
+    };
+
+} // end namespace laserscanner_sick_lms1xx
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <laserscanner_sick_lms1xx/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    laserscanner_sick_lms1xx::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
